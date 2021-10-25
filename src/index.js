6799177685d1f7cb3f08aa3cf2f51bb4210f3b5d
SunnySparks <Josediazt6@outlook.com>
2021-10-25 18:18:21 -0500

    Display frontend

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import TodoContainer from "./components/TodoContainer"
+
+//component file
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
+
