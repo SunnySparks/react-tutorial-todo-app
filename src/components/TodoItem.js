901b0d432f6343e85b6f4b901cf689d7d8d0d715
SunnySparks <Josediazt6@outlook.com>
2021-10-25 21:09:29 -0500

    Update edited items with enter

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -13,6 +13,11 @@ class TodoItem extends React.Component {
           editing: true,
         })
     }
+    handleUpdatedDone = event => {
+        if (event.key === "Enter") {
+          this.setState({ editing: false })
+        }
+    }
     render () {
     const { completed, id, title } = this.props.todo
     const completedStyle = {
@@ -31,26 +36,27 @@ class TodoItem extends React.Component {
       }
     return (
         <li className={styles.item}>
-        <div onDoubleClick={this.handleEditing} style={viewMode}>
+            <div onDoubleClick={this.handleEditing} style={viewMode}>
+                <input
+                type="checkbox"
+                className={styles.checkbox}
+                checked={completed}
+                onChange={() => this.props.handleChangeProps(id)}
+                />
+                <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+                <span style={completed ? completedStyle : null}>{title}</span>
+            </div>
             <input
-            type="checkbox"
-            className={styles.checkbox}
-            checked={completed}
-            onChange={() => this.props.handleChangeProps(id)}
+                type="text"
+                style={editMode}
+                className={styles.textInput}
+                value={title}
+                onChange={e => {
+                    this.props.setUpdate(e.target.value, id)
+                }}
+                onKeyDown={this.handleUpdatedDone}
             />
-            <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
-            <span style={completed ? completedStyle : null}>{title}</span>
-        </div>
-        <input
-            type="text" 
-            style={editMode}
-            className={styles.textInput}
-            value={title}
-            onChange={e => {
-                this.props.setUpdate(e.target.value, id)
-            }}
-        />
-    </li>
+        </li>
     )
 }
 }
