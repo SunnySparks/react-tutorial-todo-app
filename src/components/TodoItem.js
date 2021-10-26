d8c45ce2aecbbcac501e3f34c1964e12603bafc4
SunnySparks <Josediazt6@outlook.com>
2021-10-25 21:07:07 -0500

    Edit todos item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,24 +1,58 @@
 import React from "react";
 import styles from "./TodoItem.module.css";
 
-function TodoItem(props) {
-    const { completed, id, title } = props.todo;
+class TodoItem extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            editing: false,
+        };
+    };
+    handleEditing = () => {
+        this.setState({
+          editing: true,
+        })
+    }
+    render () {
+    const { completed, id, title } = this.props.todo
     const completedStyle = {
         fontStyle: "italic",
         color: "#595959",
         opacity: 0.4,
         textDecoration: "line-through",
       }
-  return <li className={styles.item}>
-  <input
-    type="checkbox"
-    className={styles.checkbox}
-    checked={completed}
-      onChange={() => props.handleChangeProps(id)}
-    />
-    <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
-    <span style={completed ? completedStyle : null}>{title}</span>
-</li>
+      let viewMode = {}
+      let editMode = {}
+      
+      if (this.state.editing) {
+        viewMode.display = "none"
+      } else {
+        editMode.display = "none"
+      }
+    return (
+        <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+            <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={completed}
+            onChange={() => this.props.handleChangeProps(id)}
+            />
+            <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+            <span style={completed ? completedStyle : null}>{title}</span>
+        </div>
+        <input
+            type="text" 
+            style={editMode}
+            className={styles.textInput}
+            value={title}
+            onChange={e => {
+                this.props.setUpdate(e.target.value, id)
+            }}
+        />
+    </li>
+    )
+}
 }
 
 export default TodoItem;
