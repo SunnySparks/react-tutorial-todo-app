c24d9a2f8eb4702ae240446f2facecca1325de33
SunnySparks <Josediazt6@outlook.com>
2021-10-25 20:15:05 -0500

    change code to destructuring

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./TodoItem.module.css";
 
 function TodoItem(props) {
+    const { completed, id, title } = props.todo;
     const completedStyle = {
         fontStyle: "italic",
         color: "#595959",
@@ -12,15 +13,11 @@ function TodoItem(props) {
   <input
     type="checkbox"
     className={styles.checkbox}
-    checked={props.todo.completed}
-    onChange={() => props.handleChangeProps(props.todo.id)}
-  />
-  <button onClick={() => props.deleteTodoProps(props.todo.id)}>
-    Delete
-  </button>
-  <span style={props.todo.completed ? completedStyle : null}>
-    {props.todo.title}
-  </span>
+    checked={completed}
+      onChange={() => props.handleChangeProps(id)}
+    />
+    <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
+    <span style={completed ? completedStyle : null}>{title}</span>
 </li>
 }
 
