bbfeb7b89f7eec5c2218383622ac4aa7258811db
SunnySparks <Josediazt6@outlook.com>
2021-10-25 20:02:25 -0500

    Styling with CSS modules

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,19 @@
 import React from "react";
+import styles from "./TodoItem.module.css";
 
 function TodoItem(props) {
-  return <li><input 
-                type="checkbox" 
-                checked={props.todo.completed}
-                onChange={() => props.handleChangeProps(props.todo.id)}
-            />
-            <button onClick={() => props.deleteTodoProps(props.todo.id)}>
-                Delete
-            </button>
-            {props.todo.title}</li>
+  return <li className={styles.item}>
+    <input
+      type="checkbox"
+      className={styles.checkbox}
+      checked={props.todo.completed}
+      onChange={() => props.handleChangeProps(props.todo.id)}
+    />
+    <button onClick={() => props.deleteTodoProps(props.todo.id)}>
+      Delete
+    </button>
+    {props.todo.title}
+  </li>
 }
 
 export default TodoItem;
