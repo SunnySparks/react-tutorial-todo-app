ceaf7291c2d073cbcff6080c80701022662524db
SunnySparks <Josediazt6@outlook.com>
2021-10-26 11:55:26 -0500

    Nested and Dynamic Routing

--- src/functionBased/pages/About.js
+++ src/functionBased/pages/About.js
@@ -1,6 +1,25 @@
-import React from "react"
+import React from "react";
+import { Link, useRouteMatch, Route } from "react-router-dom";
+import SinglePage from "./SinglePage";
 
-const About = () => {
-  return <div>hello from about page</div>
+const About = (props) => {
+    console.log(props)
+    const { url, path } = useRouteMatch()
+    return (
+        <div>
+            <ul>
+            <li>
+                <Link to={`${url}/about-app`}>About App</Link>
+            </li>
+            <li>
+                <Link to={`${url}/about-author`}>About Author</Link>
+            </li>
+            </ul>
+            <Route path={`${path}/:slug`}>
+            <SinglePage />
+            </Route>
+        </div>
+    )
+    return <div>hello from about page</div>;
 }
-export default About
+export default About;
