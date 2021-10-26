d9f6f7ee975c415f538e82e97e6da05d28247b45
SunnySparks <Josediazt6@outlook.com>
2021-10-25 19:06:11 -0500

    Handling react form

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -2,17 +2,25 @@ import React, { Component } from "react";
 
 class InputTodo extends Component {
     state = {
-        title: ""
+        title: "",
+        fName: "",
+        lastName: "",
     };
     onChange = e => {
         this.setState({
-            title: e.target.value
+            [e.target.name]: e.target.value,
         });
     };
     render() {
         return (
         <form>
-            <input type="text" placeholder="Add Todo..." value={this.state.title} onChange={this.onChange} />
+            <input
+                type="text"
+                placeholder="Add Todo..."
+                value={this.state.title}
+                name="title"
+                onChange={this.onChange}
+            />
             <button>Submit</button>
         </form>
         )
